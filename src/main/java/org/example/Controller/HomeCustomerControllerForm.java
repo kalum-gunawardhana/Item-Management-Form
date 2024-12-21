f4839ef78de0ee46d986c7b0ff53a997eb458030
Kalum Gunawardhana <kguna415@gmail.com>
2024-12-21 21:06:57 +0530

    commit

---
+++ src/main/java/org/example/Controller/HomeCustomerControllerForm.java
@@ -0,0 +1,25 @@
+package org.example.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomeCustomerControllerForm {
+    public void btnAddOnView(ActionEvent actionEvent) throws IOException {
+        System.out.println("add");
+        Stage stage=new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/add_customer_form.fxml"))));
+        stage.show();
+    }
+
+    public void btnViewOnAction(ActionEvent actionEvent) {
+        System.out.println("view");
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+        System.out.println("delete");
+    }
+}
